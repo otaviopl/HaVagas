4e35a824a1ce828e97c1400c036c0813916f5048
otaviopl <otavio.pereira.lopes@gmail.com>
2025-09-05 07:55:17 -0300

    fix theme config

--- app/src/main/java/com/example/havagas/MainActivity.kt
+++ app/src/main/java/com/example/havagas/MainActivity.kt
@@ -2,21 +2,19 @@ package com.example.havagas
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 import com.example.havagas.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Inicializa o ViewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Exemplo: quando o usuário digitar nome e email e sair do campo
         binding.emailEt.setOnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
                 val nome = binding.nomeEt.text.toString()
